1a4b3daee80aea7930cff85421c4062ef1da418f
Strelix <treyww2008@hotmail.com>
2022-11-30 14:24:35 +0000

    -> Interface Additions

--- UI/Popup.py
+++ UI/Popup.py
@@ -4,10 +4,13 @@ from customtkinter import LEFT, RIGHT, TOP, BOTTOM
 
 class Popup:
     def __init__(self, main):
-        self.container = customtkinter.CTkFrame(master=main.main.container, width=main.main.container.winfo_screenwidth() * 0.25,
-                                                     height=main.main.container.winfo_screenheight() * 0.125,
-                                                     corner_radius=20, background='green')
-        self.container.pack(side = LEFT, padx=15, pady=(0, main.main.container.winfo_screenheight() * 0.7))
+        main_dimensions = main.winfo_screenwidth(), main.winfo_screenheight()
+
+
+        self.container = customtkinter.CTkFrame(master=main, width=main_dimensions[0] * 0.25,
+                                                     height=main_dimensions[1] * 0.125,
+                                                     corner_radius=0, fg_color='#333333')
+        self.container.place(x=(main_dimensions[0] * 0.05), y=(main_dimensions[1] * 0.1)) # .pack(side = LEFT, padx=15, pady=(0, main.main.container.winfo_screenheight() * 0.7))
         self.container.pack_propagate(False)
 
         self.heading = customtkinter.CTkLabel(master=self.container, text='POPUP MESSAGE',
